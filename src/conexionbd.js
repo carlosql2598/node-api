919c3d7b2ac07b331a08412658541568bd583fb1
Kir_Argomedo <kirargomedo1@gmail.com>
2021-05-26 07:08:33 -0500

    Act. de config de BD

--- src/conexionbd.js
+++ src/conexionbd.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
-
+// mysql://b2de383794b057:b5837737@us-cdbr-east-03.cleardb.com/heroku_f79b6a9307229c1?
 const coneccion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'bdcapstone'
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b2de383794b057',
+    password: 'b5837737',
+    database: 'heroku_f79b6a9307229c1'
 });
 
 coneccion.connect( error => {
