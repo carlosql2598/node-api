029341216b9c6a73233952a509915dd57af4bacb
Kir_Argomedo <kirargomedo1@gmail.com>
2021-05-26 09:59:49 -0500

    Act. en conexión

--- src/conexionbd.js
+++ src/conexionbd.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+const port = require('./constantes');
 // mysql://b2de383794b057:b5837737@us-cdbr-east-03.cleardb.com/heroku_f79b6a9307229c1?
 const coneccion = mysql.createConnection({
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'b2de383794b057',
-    password: 'b5837737',
-    database: 'heroku_f79b6a9307229c1'
+    host:  port.hostDev,
+    user: port.userDev,
+    password: 'root',
+    database: 'bdcapstone'
 });
 
 coneccion.connect( error => {
--- src/constantes.js
+++ src/constantes.js
@@ -1,5 +1,11 @@
 const constantes = {
-    "origin": "http://localhost:4200"
+    "origin": "http://localhost:4200",
+    "hostDev": '127.0.0.1',
+    "userDev": 'root',
+    "hostPro": "us-cdbr-east-03.cleardb.com",
+    "userProd": 'b2de383794b057',
+    "passwordProd":'b5837737',
+    "databaseProd" : 'heroku_f79b6a9307229c1'
 }
 
 module.exports = constantes;
