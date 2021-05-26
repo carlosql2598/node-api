03483dd9f260a351fdf730523113c63c3b20a92e
Kir_Argomedo <kirargomedo1@gmail.com>
2021-05-26 10:40:25 -0500

    Prueba 1

--- src/conexionbd.js
+++ src/conexionbd.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const port = require('./constantes');
 // mysql://b2de383794b057:b5837737@us-cdbr-east-03.cleardb.com/heroku_f79b6a9307229c1?
 const coneccion = mysql.createConnection({
-    host:  port.hostDev,
-    user: port.userDev,
-    password: 'root',
-    database: 'bdcapstone'
+    host:  port.hostPro,
+    user: port.userProd,
+    password: port.passwordProd,
+    database: port.databaseProd
 });
 
 coneccion.connect( error => {
