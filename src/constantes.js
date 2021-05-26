2a561bcbc6792d2a97819af17e2c6588191f04b5
Kir_Argomedo <kirargomedo1@gmail.com>
2021-05-26 11:44:11 -0500

    Act. prueba 8

--- src/constantes.js
+++ src/constantes.js
@@ -2,7 +2,7 @@ const constantes = {
     "origin": "http://localhost:4200",
     "hostDev": '127.0.0.1',
     "userDev": 'root',
-    "hostPro": " bhoeomzuopzhb4vhx6wa-mysql.services.clever-cloud.com",
+    "hostPro": "bhoeomzuopzhb4vhx6wa-mysql.services.clever-cloud.com",
     "userProd": 'us4506qjxvm11soe',
     "passwordProd":'jMtRmSJS4jXKhPxFls0R',
     "databaseProd" : 'bhoeomzuopzhb4vhx6wa'
