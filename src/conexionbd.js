c0e499fe79c22adbe63022bfdf1ee69d0c47ef64
Kir_Argomedo <kirargomedo1@gmail.com>
2021-06-05 09:29:47 -0500

    Nueva imp. de conexión con la BD

--- src/conexionbd.js
+++ src/conexionbd.js
@@ -1,30 +1,87 @@
 const mysql = require('mysql');
 const port = require('./constantes');
 // mysql://b2de383794b057:b5837737@us-cdbr-east-03.cleardb.com/heroku_f79b6a9307229c1?
+
+//Configuración
 const dbconfig = {
-    host:  port.hostPro,
-    user: port.userProd,
-    password: port.passwordProd,
-    database: port.databaseProd
+    host:  port.hostProduccion,
+    user: port.userProduccion,
+    password: port.passwordProduccion,
+    database: port.databaseProduccion
 };
 
-function connectToDatabase(){
 
-    let coneccion = mysql.createConnection(dbconfig);
+//Creación de la conexión
+var connection = mysql.createConnection(dbconfig);
 
-    coneccion.connect( error => {
-        if(error) throw error;
+//Establecer una nueva conexión
+connection.connect( error => {
+    if(error){
+        console.log('Ocurrió un error en la conexión con la BD');
+        connection = reconnect(connection);
+    } else{ 
         console.log('Conexión exitosa con la BD');
+    }
+    
+});
+
+
+//Reconectando
+function reconnect(connection){
+    console.log('Reconectando conexión con la Base de datos.');
+
+    //Destruyendo la conexión
+    if(connection) connection.destroy();
+
+    //Creando una nueva conexión
+    var connection = mysql.createConnection(dbconfig);
+
+    connection.connect(error => {
+        if(error){
+            setTimeout(reconnect, 2000);
+        }else{
+            console.log('Reconectado, nueva conexión estable.');
+            return connection;
+        }
     });
 
-    coneccion.on('error', (err) => {
-        console.log('Error connecting to the database');
-        console.log(err.code + ' : ' + err.message);
-        setTimeout(connectToDatabase, 3000);
-    });
-
-    return coneccion;
 }
 
-let coneccion = connectToDatabase();
-module.exports = coneccion;
+
+//Escuchando errores. 
+connection.on( 'error', function(err){
+
+    //- El servidor cierra la conexión.
+    if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+        console.log("/!\\ No se puede establecer una conexión con la Base de Datos. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Conexión cerrando.
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+        console.log("/!\\ No se puede establecer una conexión con la Base de Datos. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Error fatal: la variable de conexión debe volver a crearse.
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+        console.log("/!\\ No se puede establecer una conexión con la Base de Datos. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Error porque ya se está estableciendo una conexión.
+    else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+        console.log("/!\\ No se puede establecer una conexión con la Base de Datos. /!\\ ("+err.code+")");
+    }
+
+    //- Otros casos.
+    else{
+        console.log("/!\\ No se puede establecer una conexión con la Base de Datos. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+} );
+
+
+
+
+module.exports = connection;
--- src/constantes.js
+++ src/constantes.js
@@ -1,11 +1,12 @@
 const constantes = {
     "origin": "http://localhost:4200",
-    "hostDev": '127.0.0.1',
-    "userDev": 'root',
-    "hostPro": "bhoeomzuopzhb4vhx6wa-mysql.services.clever-cloud.com",
-    "userProd": 'us4506qjxvm11soe',
-    "passwordProd":'jMtRmSJS4jXKhPxFls0R',
-    "databaseProd" : 'bhoeomzuopzhb4vhx6wa'
+    "hostLocal": '127.0.0.1',
+    "userLocal": 'root',
+    "databaseLocal": 'bdcapstone',
+    "hostProduccion": "bhoeomzuopzhb4vhx6wa-mysql.services.clever-cloud.com",
+    "userProduccion": 'us4506qjxvm11soe',
+    "passwordProduccion":'jMtRmSJS4jXKhPxFls0R',
+    "databaseProduccion" : 'bhoeomzuopzhb4vhx6wa'
 }
 
 module.exports = constantes;
